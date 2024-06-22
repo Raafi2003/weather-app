468d8221d39f3ceba0648b5ed8a9617bfb09d6cc
manibalaji003 <manibalaji22003@gmail.com>
2024-06-22 17:24:12 +0530

    weather app

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const searchBar =  document.getElementById('search-bar');
 const placeDoc = document.getElementById('place-text')
 const searchIcon = document.getElementsByClassName('search-icon')
-
-
+const temperature=document.getElementById('temperature');
+const cityName=document.getElementById("cityname");
+const humidity= document.getElementById('humidity');
+const wind=document.getElementById('wind');
+//let datavar="";
 const getWeather= async (place)=>{
     let unit = 'metric'; //celsius
     let API = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=40423493acf5b9dbf2dc3e6190651ec1&units=${unit}`
-    let res= await fetch(API);
-    res=res.json()
-    console.log(res)
+    await fetch(API).then((res)=>{
+        return res.json();
+    
+    }).then((data)=>{
+        let responsedata=data;
+       // console.log(responsedata);
+        cityName.innerText=responsedata.name;
+        temperature.innerText=responsedata.main.temp;
+        humidity.innerText=responsedata.main.humidity;
+        wind.innerText=responsedata.wind.speed;
+    })
+    
+    //console.log(res)
 }
 
 const searchHandler=(place)=>{
